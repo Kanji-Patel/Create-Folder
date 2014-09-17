54a58c9c4de7d28c696f44376d32cd5d2b3d3d02
kanji11 <kanji11@users.noreply.github.com>
2014-09-17 16:47:58 +0530

    Added

---
+++ frmCreateFolders.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCreateFolders
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCreate = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.txtDirectoryPath = New System.Windows.Forms.TextBox
+        Me.btnCreateAndOpen = New System.Windows.Forms.Button
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel
+        Me.SuspendLayout()
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(608, 38)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCreate.TabIndex = 14
+        Me.btnCreate.Text = "Create"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Folder Path"
+        '
+        'txtDirectoryPath
+        '
+        Me.txtDirectoryPath.Location = New System.Drawing.Point(98, 12)
+        Me.txtDirectoryPath.Name = "txtDirectoryPath"
+        Me.txtDirectoryPath.Size = New System.Drawing.Size(585, 20)
+        Me.txtDirectoryPath.TabIndex = 12
+        '
+        'btnCreateAndOpen
+        '
+        Me.btnCreateAndOpen.Location = New System.Drawing.Point(470, 38)
+        Me.btnCreateAndOpen.Name = "btnCreateAndOpen"
+        Me.btnCreateAndOpen.Size = New System.Drawing.Size(132, 23)
+        Me.btnCreateAndOpen.TabIndex = 15
+        Me.btnCreateAndOpen.Text = "Create && Open"
+        Me.btnCreateAndOpen.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(98, 39)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(142, 13)
+        Me.LinkLabel1.TabIndex = 16
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "www.totalyinfo.blogspot.com"
+        '
+        'frmCreateFolders
+        '
+        Me.AcceptButton = Me.btnCreate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(688, 65)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.btnCreateAndOpen)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtDirectoryPath)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "frmCreateFolders"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Create Folder"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCreate As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtDirectoryPath As System.Windows.Forms.TextBox
+    Friend WithEvents btnCreateAndOpen As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+End Class
